9fd004335da1b89bcab6c254ffc407c19f050676
Ihor Dovhoshliubnyi <DJumpJet77@gmail.com>
2018-03-22 13:47:13 +0200

    Start new activity. Create stub for converter.

--- Calculator/app/src/main/java/ua/edu/viti/calculator/Calculator.java
+++ Calculator/app/src/main/java/ua/edu/viti/calculator/Calculator.java
@@ -1,9 +1,12 @@
 package ua.edu.viti.calculator;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -367,5 +370,18 @@ public class Calculator extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, ConverterActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ Calculator/app/src/main/java/ua/edu/viti/calculator/ConverterLogic.java
@@ -0,0 +1,14 @@
+package ua.edu.viti.calculator;
+
+/**
+ * Created by Ihor Dovhoshliubnyi on 3/22/18.
+ */
+
+public class ConverterLogic {
+
+    public double convertMass(double value) {
+
+
+        return 0;
+    }
+}
